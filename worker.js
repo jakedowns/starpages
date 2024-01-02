fdcf5d5b00795df2334a5f1a2da7863bc6892ef0
jake downs <mail@jakedowns.com>
2024-01-02 18:52:27 -0500

    working on subframe playback

--- worker.js
+++ worker.js
@@ -7,8 +7,13 @@ Module.onRuntimeInitialized = function() {
 
 // Define a function to handle messages from the main thread
 onmessage = function(e) {
-    // Perform computations using the Wasm module
-    const result = Module._add(...e.data);
-    // Send the result back to the main thread
-    postMessage(result);
+    // Check if e.data is iterable before using spread syntax
+    if (Symbol.iterator in Object(e.data)) {
+        // Perform computations using the Wasm module
+        const result = Module._add(...e.data);
+        // Send the result back to the main thread
+        postMessage(result);
+    } else {
+        console.error('TypeError: Spread syntax requires iterable[Symbol.iterator] to be a function');
+    }
 };
