9bff891b70eea483676e7584996503ddd9069f14
jake downs <mail@jakedowns.com>
2024-01-07 19:14:14 -0500

    bloop

--- neat.js
+++ neat.js
@@ -11742,6 +11742,9 @@ const SO_DO_IT = ()=>{
 // central command definitions
 const InvokableCommands = {
 
+    "pixel mixer": "./webgl2.html",
+    "interactive 3d webgl video mandlebulb": "./webgl2.html",
+    "webgl2": "./webgl2.html",
     "frosty": "./webgl2.html",
 
     "./new-year-2024.html": RES,
